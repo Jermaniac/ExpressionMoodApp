400b2f863d12af995bc57e143dfde3038c2d2186
Jermaniac <stalin_bnk@hotmail.com>
2021-08-09 17:24:47 +0200

    clean code

--- app.py
+++ app.py
@@ -1,59 +1,32 @@
-#!/usr/bin/env python
-# coding: utf-
-# import tensorflow
-# # ### Imports de Keras: incluye definiciones de modelo a partir de json, optimizadores de la CNN, funciones para pasar a hot encoding y funciones para que funcione la CNN.
 from tensorflow.keras.models import load_model
-# from tensorflow.keras.optimizers import *
-
 
 # ### Import para utilizar numpy arrays.
 import numpy as np
 
-
-# ### Imports de Flask para que funcionen las peticiones POST al servidor.
+# ### Imports de Flask para que funcionen las peticiones POST al servidor y habilitar CORS
 
 from flask import Flask, request
-
 from flask_cors import CORS
-
-# from bottle import Bottle,route, run, request,response
-
 
 # ### Imports de PIL para abrir la foto y poder manipularla.
 
-
 from PIL import Image
-
 
 # ### Imports para que funcionen los archivos temporales.
 
 import tempfile
 
-
 # ### VARIABLES GLOBALES
-
 
 expressions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
 
-
-# Direccion ip en donde se realizan las peticiones
-
-
+# Direccion ip en donde se realizan las peticiones (0.0.0.0) para que funcione en la nube
 
 ip = '0.0.0.0'
 
-
 # # PROGRAMA PRINCIPAL
 
-# Este programa funciona usando una red neuronal convolucional para realizar las predicciones.
-# El programa es controlado mediante unas variables para indicar si se desea realizar algunas acciones que no son obligatorias para el funcionamiento del programa, pero que ayudan a entender mejor el funcionamiento.
-# Es por ello, que aunque el programa funcione con una CNN, se ha incorporado también RandomForest con fines de aprendizaje. De esta forma, se ha podido comprobar qué método es mejor para clasificar imágenes.
-# Las imágenes que llegan como peticion del cliente se han adecuado para que las pueda interpretar la CNN.
-
-
 # ### Función que prepara la foto recibida para que la CNN pueda interpretarla.
-
-
 
 def prepare_photo(imagen):
     # convertimos la foto a gris y a tamaño 48x48
@@ -63,21 +36,14 @@ def prepare_photo(imagen):
     x = x / 255
     return x
 
-
 # ### Función para realizar la predicción correspondiente.
 
-
-
 def do_prediction(ImArray):
-
     porcentajes = model.predict(ImArray).tolist()
     #retornamos los datos de la prediccion en formato json
     return prepare_json(porcentajes)
 
-
 # ### Función que prepara los resultados para devolver un json al cliente.
-
-
 
 def prepare_json(porcentajes):
     jsons = {"expressions":[]}
@@ -90,7 +56,6 @@ def prepare_json(porcentajes):
     # devolvemos un json con todas las probabilidades
     return jsons
 
-
 # # PROGRAMA
 
 # cargamos el archivo h5
@@ -101,8 +66,6 @@ print("El modelo ha sido cargado.")
 
 
 # ### Peticion POST del lado del servidor.
-
-
 
 app = Flask(__name__)
 CORS(app)
@@ -120,14 +83,10 @@ def do_upload():
     resultado = do_prediction(imagen_lista)
     # cerramos el archivo temporal
     temporal.close()
-
     print(resultado)
-
     return resultado
-
 
 # ### Ejecutamos el servidor para que se puedan realizar peticiones.
 
 if __name__ == "__main__":
     app.run(host=ip, port=9004)
-
