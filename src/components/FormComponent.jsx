d64277561cdd882395edbe84db9c567591d9b86f
Diana Perdomo <stalin_bnk@hotmail.com>
2024-01-16 00:52:21 +0100

    styles: added spinner when loading with an interval. Show a message when API call takes more than 4 seconds

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -2,7 +2,15 @@ import { useContext, useState } from "react";
 import { ExpressionContext } from "../context/expressionContext";
 import { getMood } from "../services/image-upload.service";
 
+const Spinner = () => {
+  return (
+    <div className="w-6 h-6 border-t-2 border-blue-500 border-solid rounded-full animate-spin relative inset-0 m-auto"></div>
+  );
+};
+
 const FormComponent = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [showTimeoutMessage, setShowTimeoutMessage] = useState(false);
   const [getPhotoFile, setPhotoFile] = useState({
     src: "./assets/images/blankPhoto.webp",
   });
@@ -22,16 +30,26 @@ const FormComponent = () => {
 
   const requestPredict = async (event) => {
     event.preventDefault();
-    const response = await getMood(getPhotoFile.file);
-    if (response && response.expressions) {
-      expContext.setExpressions(response.expressions);
-    }
+    let timeoutId;
+    setIsLoading(true);
+    timeoutId = setTimeout(() => {
+      setShowTimeoutMessage(true);
+    }, 4000);
+    await getMood(getPhotoFile.file)
+      .then((response) => {
+        expContext.setExpressions(response.expressions);
+      })
+      .catch((error) => console.log("Error calling API: ", error))
+      .finally(() => {
+        clearTimeout(timeoutId);
+        setIsLoading(false);
+      });
   };
 
   return (
     <div className="flex justify-center bg-black p-5" id="form">
       <form
-        className="max-w-3xl bg-gradient-to-br from-purple-800 to-indigo-800 p-6 rounded-xl shadow-lg hover:shadow-xl transition duration-300 text-center"
+        className="max-w-xl bg-gradient-to-br from-purple-800 to-indigo-800 p-6 rounded-xl shadow-lg hover:shadow-xl transition duration-300 text-center"
         id="form"
       >
         <div className="p-4 text-center text-white font-bold">
@@ -42,31 +60,46 @@ const FormComponent = () => {
             Click on the image to upload a photo.
           </p>
         </div>
-        <label className="flex justify-center" htmlFor="photo">
-          <input
-            className="hidden"
-            type="file"
-            name="photo"
-            id="photo"
-            accept="image/*"
-            onChange={selectPhoto}
-          ></input>
-          <img
-            className="p-6 w-80 h-96"
-            src={getPhotoFile.src}
-            alt="selectedImage"
-          />
-        </label>
+        <div className="flex justify-center">
+          <label className="w-fit cursor-pointer" htmlFor="photo">
+            <input
+              className="hidden"
+              type="file"
+              name="photo"
+              id="photo"
+              accept="image/*"
+              onChange={selectPhoto}
+            ></input>
+            <img
+              className="p-6 w-90 h-96"
+              src={getPhotoFile.src}
+              alt="selectedImage"
+            />
+          </label>
+        </div>
         {getPhotoFile.file && (
           <button
-            className="bg-white text-gray-800 font-bold py-2 px-4 rounded-full hover:bg-gray-200 transition duration-300"
+            className="w-2/4 bg-white text-gray-800 font-bold py-2 px-4 rounded-xl hover:bg-gray-200 transition duration-300"
             id="buttonSubmit"
             onClick={requestPredict}
-            disabled={!getPhotoFile.file}
+            disabled={isLoading || !getPhotoFile.file}
           >
-            PREDICT THIS PHOTO
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              <span className="text-black">PREDICT THIS PHOTO</span>
+            )}
           </button>
         )}
+        <div className="p-4 text-center text-red-500 font-bold">
+          {showTimeoutMessage && (
+            <p className="text-xl" id="timeout_message">
+              Note: The first upload request may experience a delay due to
+              server initialization. If this occurs, please try again after a
+              minute.
+            </p>
+          )}
+        </div>
       </form>
     </div>
   );
